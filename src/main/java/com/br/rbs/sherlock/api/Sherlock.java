9e8af363e790afd9eceb3122bdc331e5bfe11467
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-08-24 17:43:18 -0300

    ignored class files, also packaging files

---
+++ src/main/java/com/br/rbs/sherlock/api/Sherlock.java
@@ -0,0 +1,11 @@
+package com.br.rbs.sherlock.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: helmed
+ * Date: 8/24/13
+ * Time: 4:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sherlock {
+}
