3f544f882acf450f701f966a39dd53b00232e822
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-08-24 17:46:11 -0300

    Servlet and JSP samples

--- src/main/java/com/br/rbs/sherlock/api/HelloServlet.java
+++ src/main/java/com/br/rbs/sherlock/api/HelloServlet.java
@@ -1,11 +1,23 @@
 package com.br.rbs.sherlock.api;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- * Created with IntelliJ IDEA.
+ * ServletTest with jetty
  * User: helmed
  * Date: 8/24/13
  * Time: 4:59 PM
- * To change this template use File | Settings | File Templates.
  */
-public class HelloServlet {
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("<h1>Hello from Sherlock</h1>");
+        resp.getWriter().println("session="+req.getSession(true).getId());
+    }
 }
--- src/main/java/com/br/rbs/sherlock/api/Sherlock.java
+++ src/main/java/com/br/rbs/sherlock/api/Sherlock.java
@@ -1,11 +1,35 @@
 package com.br.rbs.sherlock.api;
 
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- * Created with IntelliJ IDEA.
- * User: helmed
+ * Main class to test Jetty Configuration
+ * User: helmedeiros
  * Date: 8/24/13
  * Time: 4:31 PM
- * To change this template use File | Settings | File Templates.
  */
-public class Sherlock {
+public class Sherlock extends AbstractHandler {
+
+    @Override
+    public void handle(String s, Request baseRequest, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Sherlock<h1>");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new Sherlock());
+
+        server.start();
+        server.join();
+    }
 }
